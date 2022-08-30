03c8929ac2f0e2b93a65079fb930c8231b04c9fd
flo-bou <florent.b@gmx.com>
2022-08-30 17:29:30 +0200

    build phase

---
+++ build-win.bat
@@ -0,0 +1,2 @@
+poetry run pyinstaller --windowed --name marovany --collect-data librosa main.py
+	@REM --paths C:\\Users\\admin\\AppData\\Local\\pypoetry\\Cache\\virtualenvs\\midiconversion-marovany-zyrvOt6a-py3.8\\Lib\\site-packages\\ ^

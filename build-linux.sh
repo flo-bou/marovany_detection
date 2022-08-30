d5814ac6be4a023810cbd839ddfe5b0a5e124121
flo-bou <florent.b@gmx.com>
2022-08-30 22:32:13 +0200

    linux build

---
+++ build-linux.sh
@@ -0,0 +1,3 @@
+# lancement : . build-linux.sh
+poetry run pyinstaller --windowed --name marovany --collect-data librosa main.py
+# --paths C:\\Users\\admin\\AppData\\Local\\pypoetry\\Cache\\virtualenvs\\midiconversion-marovany-zyrvOt6a-py3.8\\Lib\\site-packages\\ ^
--- build-win.bat
+++ build-win.bat
@@ -1,2 +1,3 @@
+@REM lancement : build-win.bat
 poetry run pyinstaller --windowed --name marovany --collect-data librosa main.py
 	@REM --paths C:\\Users\\admin\\AppData\\Local\\pypoetry\\Cache\\virtualenvs\\midiconversion-marovany-zyrvOt6a-py3.8\\Lib\\site-packages\\ ^
